313d301dee852feac784370f0caac3a7ffe9b037
Ali <pantsinurmouthbutitisstwisted@gmail.com>
2025-03-16 15:13:20 +0200

    Updated dropdown options and added links to the bot

--- index.js
+++ index.js
@@ -39,11 +39,11 @@ client.on('interactionCreate', async (interaction) => {
     let responseEmbed = new EmbedBuilder().setColor(0x0099ff);
 
     if (interaction.values[0] === 'handbook') {
-        responseEmbed.setTitle('Handbook').setDescription('[Link to the Handbook](https://example.com)');
+        responseEmbed.setTitle('Handbook').setDescription('[Link to the Handbook](https://docs.google.com/document/d/1x1ZR0G78dNa6oEx7GdflsLD3ZLzrwojEjmuDPP9a2B4/edit?usp=sharing)');
     } else if (interaction.values[0] === 'rules') {
         responseEmbed.setTitle('Rules').setDescription('Please check the #rules channel!');
     } else if (interaction.values[0] === 'uniform') {
-        responseEmbed.setTitle('Uniform').setDescription('[Uniform Link](https://example.com)');
+        responseEmbed.setTitle('Uniform').setDescription('[Uniform Link](https://www.roblox.com/catalog/85787972042536/TLM-Shirt)');
     }
 
     await interaction.reply({ embeds: [responseEmbed], ephemeral: true });
